fa21e234082bd43cbf9a994699f8c785580493b9
jacob-s-codes <jacobbshaul@gmail.com>
2024-06-18 14:34:12 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        newBlue: 'rgb(31, 51, 88)'
+      }
+    },
+    
+  },
+  plugins: [],
+}
+
