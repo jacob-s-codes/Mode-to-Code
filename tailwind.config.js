059e519863947fad950e4d3486c1a7ae94c04ea0
jacob-s-codes <jacobbshaul@gmail.com>
2024-06-24 09:15:27 -0700

    made text smaller for viewports

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content:["*.html", "*/index.html", "*/**/index.html"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'mid': '645px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
   },
   plugins: [],
 }
