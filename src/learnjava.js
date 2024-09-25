e423005a186a579265547bd448c7cb1e8b7b2744
jacob-s-codes <jacobbshaul@gmail.com>
2024-09-25 09:21:06 -0700

    added learn ai

--- src/learnjava.js
+++ src/learnjava.js
@@ -1,29 +1,23 @@
-let sleepTime = 100;
-const phrase = "Still working on this section!";
 
-const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+        const el = document.getElementById("typewriter");
+        const phrase = " build websites";
 
-const writeLoop = async() => {
-    const el = document.getElementById('typewriter');
-    while (true) {
-        await sleep(1000);
-        for (let i = 0; i < phrase.length; i++) {
-            el.innerText = phrase.substring(0, i + 1);
-            await sleep(sleepTime);
-        }
+        const sleep = (ms) => {
+            return new Promise((resolve) => setTimeout(resolve, ms));
+        };
 
-        await sleep(sleepTime * 10);
+        const writeLoop = async () => {
+            await sleep(1000);
+            for (let i = 0; i < phrase.length; i++) {
+                el.innerText = phrase.substring(0, i + 1);
+                await sleep(sleepTime);
+            }
+        };
 
-        for (let i = phrase.length; i > 0; i--) {
-            el.innerText = phrase.substring(0, i - 1);
-            await sleep(sleepTime);
-        }
-        await sleep(sleepTime * 10);
-    }
+        writeLoop();
     
-}
+
+writeLoop();
 
 document.addEventListener("DOMContentLoaded", () => {
     writeLoop();
