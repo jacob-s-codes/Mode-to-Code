6de583471d6488794c56bd8b5992963efe34bd7c
jacob-s-codes <jacobbshaul@gmail.com>
2025-04-05 17:50:25 -0700

    push new branch

--- mode-to-code/src/app/components/LenisProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client"; 
-
-import React from "react";
-import ReactLenis from "lenis/react";
-
-export default function LenisProvider({ children }: { children: React.ReactNode }) {
-  return <ReactLenis root>{children}</ReactLenis>;
-}
